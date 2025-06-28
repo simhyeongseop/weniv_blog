8642c829474a91113cf406177b77392542ff7a80
202002513 심형섭 <202002513@o.cnu.ac.kr>
2025-06-28 20:54:00 +0900

    오류수정

--- js/initData.js
+++ js/initData.js
@@ -1,111 +1,72 @@
-// GitHub API를 사용하여 폴더 내의 파일 목록 가져오기 (스키마 및 url 참고)
-// https://api.github.com/repos/paullabkorea/github_blog/contents/menu
-// https://api.github.com/repos/paullabkorea/github_blog/contents/blog
+// initData.js
+// GitHub Pages raw.githubusercontent.com을 사용하여 정적 JSON 폴더 목록 가져오기
 let blogList = [];
 let blogMenu = [];
 let isInitData = false;
 
 async function initDataBlogList() {
-    /*
-    blogList를 초기화 하기 위한 함수
-    if 로컬이라면 blogList = /data/local_blogList.json 데이터 할당
-    else if 배포상태이면 blogList = GitHub에 API 데이터 할당
-    */
-    if (blogList.length > 0) {
-        // blogList 데이터가 이미 있을 경우 다시 로딩하지 않기 위함(API 호출 최소화)
-        return blogList;
+  if (blogList.length > 0) return blogList;
+  isInitData = true;
+
+  let response;
+  if (isLocal) {
+    // 로컬 JSON 사용 (개발용)
+    response = await fetch(`${url.origin}/data/local_blogList.json`);
+  } else {
+    // URL에서 사용자 및 레포지토리 정보 추출
+    if (!siteConfig.username || !siteConfig.repositoryName) {
+      const urlConfig = extractFromUrl();
+      siteConfig.username = siteConfig.username || urlConfig.username;
+      siteConfig.repositoryName = siteConfig.repositoryName || urlConfig.repositoryName;
     }
-
-    // 데이터 초기화를 한 번 했다는 것을 알리기 위한 변수
-    isInitData = true;
-
-    if (isLocal) {
-        // 로컬 환경
-        const response = await fetch(
-            url.origin + "/data/local_blogList.json"
-        );
-        blogList = await response.json();
+    if (!localDataUsing) {
+      // Raw GitHubusercontent에서 정적 JSON 불러오기 (토큰 불필요)
+      response = await fetch(
+        `https://raw.githubusercontent.com/${siteConfig.username}/${siteConfig.repositoryName}/main/data/local_blogList.json`
+      );
     } else {
-        // GitHub 배포 상태
-        // 만약 siteConfig.username이 비어있거나 siteConfig.repositoryName이 비어 있다면 해당 값을 지정하여 시작
-        // config에서 값이 없을 경우 URL에서 추출
-        if (!siteConfig.username || !siteConfig.repositoryName) {
-            const urlConfig = extractFromUrl();
-            siteConfig.username = siteConfig.username || urlConfig.username;
-            siteConfig.repositoryName =
-                siteConfig.repositoryName || urlConfig.repositoryName;
-        }
-
-        let response;
-
-        // 배포 상태에서 GitHub API를 사용(이용자가 적을 때)
-        if (!localDataUsing) {
-            response = await fetch(
-                `https://api.github.com/repos/${siteConfig.username}/${siteConfig.repositoryName}/contents/blog`
-            );
-        } else {
-            // 배포 상태에서 Local data를 사용(이용자가 많을 때)
-            response = await fetch(
-                url.origin + `/${siteConfig.repositoryName}/data/local_blogList.json`
-            );
-        }
-        // 배포 상태에서 Local data를 사용(이용자가 많을 때)
-        blogList = await response.json();
+      // 설정에 따라 로컬 JSON 사용
+      response = await fetch(
+        `${url.origin}/${siteConfig.repositoryName}/data/local_blogList.json`
+      );
     }
+  }
 
-    // console.log(blogList);
-
-    // 정규표현식에 맞지 않는 파일은 제외하여 blogList에 재할당
-    blogList = blogList.filter((post) => {
-        const postInfo = extractFileInfo(post.name);
-        if (postInfo) {
-            return post;
-        }
-    });
-
-    blogList.sort(function (a, b) {
-        return b.name.localeCompare(a.name);
-    });
-    return blogList;
+  blogList = await response.json();
+  // 유효한 파일만 필터링
+  blogList = blogList.filter(post => extractFileInfo(post.name));
+  // 최신 순 정렬
+  blogList.sort((a, b) => b.name.localeCompare(a.name));
+  return blogList;
 }
 
 async function initDataBlogMenu() {
-    if (blogMenu.length > 0) {
-        // blogMenu 데이터가 이미 있을 경우(API 호출 최소화)
-        return blogMenu;
-    }
+  if (blogMenu.length > 0) return blogMenu;
 
-    if (isLocal) {
-        // 로컬환경
-        const response = await fetch(
-            url.origin + "/data/local_blogMenu.json"
-        );
-        blogMenu = await response.json();
+  let response;
+  if (isLocal) {
+    // 로컬 JSON 사용 (개발용)
+    response = await fetch(`${url.origin}/data/local_blogMenu.json`);
+  } else {
+    // URL에서 사용자 및 레포 정보 추출
+    if (!siteConfig.username || !siteConfig.repositoryName) {
+      const urlConfig = extractFromUrl();
+      siteConfig.username = siteConfig.username || urlConfig.username;
+      siteConfig.repositoryName = siteConfig.repositoryName || urlConfig.repositoryName;
+    }
+    if (!localDataUsing) {
+      // Raw GitHubusercontent에서 정적 JSON 불러오기
+      response = await fetch(
+        `https://raw.githubusercontent.com/${siteConfig.username}/${siteConfig.repositoryName}/main/data/local_blogMenu.json`
+      );
     } else {
-        // GitHub 배포 상태
-        // 만약 siteConfig.username이 비어있거나 siteConfig.repositoryName이 비어 있다면 해당 값을 지정하여 시작
-        // config에서 값이 없을 경우 URL에서 추출
-        if (!siteConfig.username || !siteConfig.repositoryName) {
-            const urlConfig = extractFromUrl();
-            siteConfig.username = siteConfig.username || urlConfig.username;
-            siteConfig.repositoryName =
-                siteConfig.repositoryName || urlConfig.repositoryName;
-        }
-
-        let response;
-
-        // 배포 상태에서 GitHub API를 사용(이용자가 적을 때)
-        if (!localDataUsing) {
-            response = await fetch(
-                `https://api.github.com/repos/${siteConfig.username}/${siteConfig.repositoryName}/contents/menu`
-            );
-        } else {
-            // 배포 상태에서 Local data를 사용(이용자가 많을 때)
-            response = await fetch(
-                url.origin + `/${siteConfig.repositoryName}/data/local_blogMenu.json`
-            );
-        }
-        blogMenu = await response.json();
+      // 로컬 JSON 사용
+      response = await fetch(
+        `${url.origin}/${siteConfig.repositoryName}/data/local_blogMenu.json`
+      );
     }
-    return blogMenu;
+  }
+
+  blogMenu = await response.json();
+  return blogMenu;
 }
