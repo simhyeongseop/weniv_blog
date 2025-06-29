728e50e4bc01b1641b65bbb1bf288e0f31880b9c
202002513 심형섭 <202002513@o.cnu.ac.kr>
2025-06-29 23:16:00 +0900

    수정

--- config.js
+++ config.js
@@ -19,11 +19,3 @@ const users = [
 ];
 
 const localDataUsing = false; // GitHub API를 사용하여 블로그 목록을 가져올 것이므로 false로 유지
-/*
-localDataUsing는 아직 사용하는 데이터가 아닙니다.
-1. false일 경우에도 로컬에서 live server(127.0.0.1)를 사용하면 local 데이터를 사용합니다.
-2. true일 경우 local 데이터를 사용합니다 접속자가 많을 경우 true 변경하고 local 데이터를 작성하고 사용하시길 권합니다.
-*/
-
-const postsPerPage = 9;
-let currentPage = 1;
