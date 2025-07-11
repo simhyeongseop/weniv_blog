df3b76c3c37ba629e66e1a8c96d51fc28616fe7a
202002513 심형섭 <202002513@o.cnu.ac.kr>
2025-07-11 17:18:36 +0900

    globalstyle.js 둥근 모서리 수정

--- style/globalStyle.js
+++ style/globalStyle.js
@@ -53,21 +53,21 @@ const notebookcopyButtonStyle = 'border border-lv2 copy-button bg-white rounded-
 const notebookdownloadButtonStyle = 'download-button px-5 py-[11px] mb-4 text-sm font-medium text-white bg-primary rounded-[10px] hover:bg-primary';
 
 // bloglist 목록 스타일
-const bloglistFirstCardStyle = 'lg:col-span-3 md:col-span-2 col-span-1 h-auto rounded overflow-hidden bg-white/30 backdrop-blur-lg shadow-xl transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 flex md:flex-row flex-col flex-1 md:mb-[20px] cursor-pointer';
-const bloglistFirstCardImgStyle = 'w-full object-cover object-center rounded-2xl overflow-hidden md:h-auto h-[200px] md:w-[49%] lg:w-[52%] shrink-0 mr-8';
-const bloglistFirstCardDescriptionStyle = 'text-graylv4 text-base font-normal leading-snug md:max-h-40 md:line-clamp-[7] line-clamp-3 mb-3';
+const bloglistFirstCardStyle = 'lg:col-span-3 md:col-span-2 col-span-1 h-auto rounded overflow-hidden bg-white/30 hover:bg-white/50 backdrop-blur-lg shadow-xl transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 flex md:flex-row flex-col flex-1 md:mb-[20px] cursor-pointer';
+const bloglistFirstCardImgStyle = 'w-full object-cover object-center overflow-hidden md:h-auto h-[200px] md:w-[49%] lg:w-[52%] shrink-0 mr-8';
+const bloglistFirstCardDescriptionStyle = 'text-white text-base font-normal leading-snug md:max-h-40 md:line-clamp-[7] line-clamp-3 mb-3';
 
-const bloglistCardStyle = 'lg:max-w-sm overflow-hidden bg-white/30 backdrop-blur-lg shadow-xl transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer col-span-1 w-auto';
-const bloglistCardImgStyle = 'w-full h-[200px] object-cover object-center rounded-2xl overflow-hidden';
+const bloglistCardStyle = 'lg:max-w-sm overflow-hidden bg-white/30 hover:bg-white/50 backdrop-blur-lg shadow-xl transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer col-span-1 w-auto';
+const bloglistCardImgStyle = 'w-full h-[200px] object-cover object-center overflow-hidden';
 
 const bloglistCardBodyStyle = 'py-4';
 const bloglistCardTitleStyle = 'font-bold text-2xl mb-3';
 const bloglistCardCategoryStyle = 'inline-block bg-activation text-primary md:text-sm font-medium mb-3 px-3 py-1.5 rounded-lg transition duration-200 ease-in-out hover:bg-blue-400 hover:text-white';
-const bloglistCardDescriptionStyle = 'text-graylv4 text-base font-normal leading-snug h-16 line-clamp-3 mb-3';
+const bloglistCardDescriptionStyle = 'text-white text-base font-normal leading-snug h-16 line-clamp-3 mb-3';
 const bloglistCardAuthorDivStyle = 'inline-block';
 const bloglistCardAuthorImgStyle = 'inline w-8 h-8 rounded-full object-cover object-center mr-2 border border-graylv2 overflow-hidden';
 const bloglistCardAuthorStyle = 'inline text-sm font-semibold text-black mr-2';
-const bloglistCardDateStyle = 'text-graylv3 text-sm inline-block font-normal' ;
+const bloglistCardDateStyle = 'text-white text-sm inline-block font-normal' ;
 
 // pagination 관련 스타일 (render.js의 initPagination에서 사용)
 const paginationStyle       = 'mt-20 mb-[132px] flex justify-center items-center gap-8';
